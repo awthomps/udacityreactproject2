1c6ff8e8b0d1fd8e846d37a2e76fea7e56ab91b4
Andrei Thompson <killosquirrel@gmail.com>
2018-05-01 19:17:17 -0700

    feature: Add basic routing into the react app.
    App can switch between the home page and the test category page.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Link, Route } from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Readable</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Route path='/testcategory' render={() => (<div>
+          <Link to='/'>To Home</Link>
+        </div>)}/>
+        <Route exact path='/' render={() => (<div>
+          <Link to='/testcategory'>To Test Catagory</Link>
+        </div>)}/>
       </div>
     );
   }
