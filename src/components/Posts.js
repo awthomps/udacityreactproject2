83bad289615746882e34ea08e8c92122589a7497
Andrei Thompson <killosquirrel@gmail.com>
2018-05-10 23:11:56 -0700

    feature: add basic sorting in the posts list

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -7,19 +7,53 @@ class Posts extends Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      sortByTimestamp: true,
     }
   }
 
   render() {
-    const {name, posts} = this.props;
+    const { posts } = this.props;
+    let sortedPosts = posts;
+    console.log(this.state.sortByTimestamp);
+    if(this.state.sortByTimestamp) {
+      sortedPosts.sort(this.timestampSort);
+      console.log('time');
+    } else {
+      sortedPosts.sort(this.voteScoreSort);
+      console.log('vote');
+    }
     return (
       <div>
-          {posts.map(post => {
+          <select onChange={this.setSortMethod}>
+            <option value='true' defaultValue>Sort By Date</option>
+            <option value='false'>Sort by Score</option>
+          </select>
+          {sortedPosts.map(post => {
             return <PostOverview key={`post-${post.id}`} id={post.id}/>
           })}
       </div>
     );
+  }
+  
+
+  setSortMethod = (event) => {this.setState({sortByTimestamp: event.target.value === 'true'})}
+
+  // sortMethod(postA, postB) {
+  //   if(this.state.sortByTimestamp) {
+  //     return this.timestampSort(postA, postB)
+  //   } else {
+  //     return this.voteScoreSort(postA, postB);
+  //   }
+  // }
+  
+  voteScoreSort(postA, postB) {
+    console.log(postA.voteScore + ' ' + postB.voteScore);
+    return postA.voteScore - postB.voteScore;
+  }
+  
+  timestampSort(postA, postB) {
+    console.log(postA.timestamp + ' ' + postB.timestamp);
+    return postA.timestamp - postB.timestamp;
   }
 }
 
